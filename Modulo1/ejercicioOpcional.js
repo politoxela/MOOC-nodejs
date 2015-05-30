0a175330d73d909318fc603e3d4ff468c6d9cef9
marvin <napolo.1988@gmail.com>
2015-05-29 22:05:51 -0600

    Se han agregado modulo 1 y 2.

---
+++ Modulo1/ejercicioOpcional.js
@@ -0,0 +1,3 @@
+var x = Math.PI.toFixed(8);
+var y = '\u55e8\uff0c\u4f60\u597d\u5417'
+console.log(x+'\n\n'+y);
---
+++ Modulo2/ejercicioModulo2.js
@@ -0,0 +1,28 @@
+function agenda(titulo, inic) {
+    var _titulo = titulo;
+    var _contenido = inic;
+
+    return {
+        titulo: function () {
+            return _titulo;
+        },
+        meter: function (nombre, tf) {
+            _contenido[nombre] = tf;
+        },
+        tf: function (nombre) {
+            return _contenido[nombre];
+        },
+        borrar: function (nombre) {
+            delete _contenido[nombre];
+        },
+        toJSON: function () {
+            return JSON.stringify(_contenido);
+        }
+    }
+};
+
+var amigos = agenda("Amigos", {
+    Pepe: 113278561,
+    José: 157845123,
+    Jesús: 178512355
+});
