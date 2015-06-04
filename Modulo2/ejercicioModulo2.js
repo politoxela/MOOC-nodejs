61c6b08af4ce04abe65e438a9e261a2b2831d946
marvin <napolo.1988@gmail.com>
2015-06-03 20:20:14 -0600

    ejercicio del modulo 2 Terminado.

--- Modulo2/ejercicioModulo2.js
+++ Modulo2/ejercicioModulo2.js
@@ -13,16 +13,27 @@ function agenda(titulo, inic) {
             return _contenido[nombre];
         },
         borrar: function (nombre) {
-            delete _contenido[nombre];
-        },
-        toJSON: function () {
-            return JSON.stringify(_contenido);
-        }
+        delete _contenido[nombre];
+    },
+    toJSON: function () {
+        return JSON.stringify(_contenido);
+    },
+    listar: function () {
+        var miLista = "";
+        for (var nombre in _contenido) {
+            miLista = miLista + nombre + ", " + _contenido[nombre] + " \n";
+        };
+        console.log(_lista);
     }
+}
 };
+
+
 
 var amigos = agenda("Amigos", {
     Pepe: 113278561,
     José: 157845123,
     Jesús: 178512355
 });
+
+amigos.listar();
